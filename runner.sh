5f7fbeddf796881d18aa95ad951a35beae653281
dsplab72611 <dsplab72611@gmail.com>
2024-03-28 15:48:15 +0530

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-noises=(0.1 0.2 0.3 0.4)
+# noises=(0.1 0.2 0.3 0.4)
+noises=(0.1)
 # cr_ratios=(0.1 0.2 0.3 0.4)
 cr_ratios=(0.3)
 echo "Starting"
-datasets=("Cora" "baron_human" "baron_mose" "Segerstolpe" "Zheng" "TM")
-# noises=(0.1)
-data="Zheng"
-epochs=10
+# datasets=("Cora" "Citeseer" "baron_human" "baron_mose" "Segerstolpe" "Zheng" "TM" )
+datasets=("Citeseer")
+
+epochs=30
+
 central="True"
-fed="True"
+fed="False"
+#DP privacy
+privacy='False'
+#Coarsening
+coarsen='All'
+#------------------------
 for data in "${datasets[@]}"; do
     for cr_ratio in "${cr_ratios[@]}"; do
         for noise in "${noises[@]}"; do
             echo "Running $noise"
-            python3 main.py --priv_budget $noise --data $data --output "output"/$data --epochs $epochs --central $central --fed $fed --cr_ratio $cr_ratio
+            python3 main.py --priv_budget $noise --data $data --output "output"/$data --privacy $privacy --coarsen $coarsen --epochs $epochs --central $central --fed $fed --cr_ratio $cr_ratio
         done
     done 
 done
